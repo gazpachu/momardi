12140a21b2cb41dc0a1113bb4596db6a1bb88630
Joan Mira <jmira@sapient.com>
2020-03-25 21:41:37 +0900

    backup

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,19 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-layout/gatsby-browser.js'),
+      options: {"plugins":[],"component":"/Users/gazpachu/Dev/Web/momardi/src/layouts/index.js"},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-catch-links/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"Momardi - Tuesday Gutierrez","short_name":"Momardi","start_url":"/index.html","background_color":"white","theme_color":"#1AA0CD","display":"standalone","icons":[{"src":"/icons/icon-48x48.png","sizes":"48x48","type":"image/png"},{"src":"/icons/icon-96x96.png","sizes":"96x96","type":"image/png"},{"src":"/icons/icon-144x144.png","sizes":"144x144","type":"image/png"},{"src":"/icons/icon-192x192.png","sizes":"192x192","type":"image/png"},{"src":"/icons/icon-256x256.png","sizes":"256x256","type":"image/png"},{"src":"/icons/icon-384x384.png","sizes":"384x384","type":"image/png"},{"src":"/icons/icon-512x512.png","sizes":"512x512","type":"image/png"}]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[],"trackingId":"UA-268014-11"},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
